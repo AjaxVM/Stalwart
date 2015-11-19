ab9a27fcc09b82746605d68d456f7895090e9449
Matthew Roe <mroe@healthgrades.com>
2015-11-19 13:58:45 -0600

    maybe it works now?

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,21 @@
 
 module.exports = function(grunt) {
     // Load plugin
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
 
     // Project configuration.
+    // grunt.initConfig({
+    //     qunit: {
+    //         all: ['test/test-stalwart.html']
+    //     }
+    // });
     grunt.initConfig({
-        jshint: {
-            all: ['*.js', 'stalwart/*.js', 'test/*.js']
-        },
         qunit: {
-            all: ['test/test-stalwart.html']
+            src: ['tests/test-stalwart.html']
         }
     });
 
     // Task to run tests
-    grunt.registerTask('test', ['jshint', 'qunit']);
-    grunt.registerTask('default', ['test']);
+    grunt.registerTask('test', ['qunit:src']);
+    grunt.registerTask('travis', ['test']);
 };
