798588776e30c497023ff7071e8345c28d189fff
Matthew Roe <mroe@healthgrades.com>
2015-11-20 11:41:54 -0600

    Initial handle module

---
+++ stalwart/handle.js
@@ -0,0 +1,14 @@
+/*
+  Copyright 2015, Matthew Roe, under MIT license
+*/
+
+
+sW.Handle = {};
+sW.Module(sW.Handle, function(){
+    //TODO, bind handlers to values, ie <div sw-handler="xxx">
+    //or <div sw-handlers="xxx,yyy,zzz">
+
+    //or do something similar to angular directives?
+    //<div my-handle></div>
+    //sW.Handle.Handler('my-handle', function(element){})
+});
